d312e845912d27f90bad40aeccf2c40f6fec2e5d
Cinnamon <dwia.for.live@gmail.com>
2023-02-03 16:25:01 +0700

    fix maybe

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,33 @@
 const YEAR = new Date().getFullYear()
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.jsx',
-})
- 
-module.exports = withNextra()
+
+
+export default {
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      <time>{YEAR}</time> © Cinnamon.
+      <a href="/feed.xml">RSS</a>
+      <style jsx>{`
+        a {
+          float: right;
+        }
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  ),
+  darkMode: true,
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Cinnamon'
+      }
+    }
+  }
+}
+
+
