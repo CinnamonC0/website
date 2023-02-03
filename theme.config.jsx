ffcf1e30eed9d42a36f7779cede4b324d54fc1f5
Cinnamon <dwia.for.live@gmail.com>
2023-02-03 16:15:35 +0700

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,12 +1,30 @@
 export default {
-    useNextSeoProps() {
-      const { asPath } = useRouter()
-      if (asPath !== '/') {
-        return {
-          titleTemplate: '%s – Cinnamon'
+        footer: (
+          <small style={{ display: 'block', marginTop: '8rem' }}>
+            <time>{YEAR}</time> © Cinnamon.
+            <a href="/feed.xml">RSS</a>
+            <style jsx>{`
+              a {
+                float: right;
+              }
+              @media screen and (max-width: 480px) {
+                article {
+                  padding-top: 2rem;
+                  padding-bottom: 4rem;
+                }
+              }
+            `}</style>
+          </small>
+        ),
+        darkMode: true,
+        useNextSeoProps() {
+          const { asPath } = useRouter()
+          if (asPath !== '/') {
+            return {
+              titleTemplate: '%s – Cinnamon'
+            }
+          }
         }
       }
-    }
-  }
 
-let darkMode = false;
+
